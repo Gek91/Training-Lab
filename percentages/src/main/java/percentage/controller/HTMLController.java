6b4c84d91d8de6bd32551c51385ebb7e318a7ec4
Gek91 <gek.pandini@gmail.com>
2023-03-14 09:52:09 +0100

    weight table html endpoint implementation

--- percentages/src/main/java/percentage/controller/HTMLController.java
+++ percentages/src/main/java/percentage/controller/HTMLController.java
@@ -3,15 +3,19 @@ package percentage.controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.math.BigDecimal;
+
 @Controller
-public class HTMLController {
+public interface HTMLController {
 
 	@GetMapping(path = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
 	@ResponseBody
-	public String welcomeAsHTML() {
-		return "<html>\n" + "<header><title>Welcome</title></header>\n" +
-				"<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
-	}
+	String welcomeAsHTML();
+
+	@GetMapping(path= "/weights-table/{rm}", produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	String getWeightTableGiven1RMValue(@PathVariable("rm") String rm);
 }
---
+++ percentages/src/main/java/percentage/controller/impl/HTMLControllerImpl.java
@@ -0,0 +1,35 @@
+package percentage.controller.impl;
+
+import percentage.calculation.service.PercentageCalculationService;
+import percentage.controller.HTMLController;
+import percentage.controller.utils.WeightTableHTMLBuilder;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public class HTMLControllerImpl implements HTMLController {
+
+	private PercentageCalculationService percentageCalculationService;
+	private WeightTableHTMLBuilder weightTableHTMLBuilder;
+
+	public HTMLControllerImpl(PercentageCalculationService percentageCalculationService, WeightTableHTMLBuilder weightTableHTMLBuilder) {
+		this.percentageCalculationService = percentageCalculationService;
+		this.weightTableHTMLBuilder = weightTableHTMLBuilder;
+	}
+
+	@Override
+	public String welcomeAsHTML() {
+		return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+				"<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+	}
+
+	@Override
+	public String getWeightTableGiven1RMValue(String rm) {
+
+		BigDecimal rmValue = new BigDecimal(rm);
+
+		Map<BigDecimal, BigDecimal> percentagesvValueMap = percentageCalculationService.calculateStandardPercentagesFrom1RM(rmValue);
+
+		return weightTableHTMLBuilder.buildHTML(percentagesvValueMap, rmValue);
+	}
+}
