eb534d0dd917ef5b36570879a4879d6ec67e65c6
Gek91 <gek.pandini@gmail.com>
2023-06-20 21:12:42 +0200

    fix

--- record/src/main/java/traininglab/personalrecord/application/PersonalRecordApplicationService.java
+++ record/src/main/java/traininglab/personalrecord/application/PersonalRecordApplicationService.java
@@ -1,14 +1,17 @@
 package traininglab.personalrecord.application;
 
-import traininglab.rest.data.CreateRecordRequestDTO;
-import traininglab.rest.data.ExerciseDTO;
-import traininglab.rest.data.RecordEntryDTO;
+import traininglab.personalrecord.application.data.CreateRecordRequestDTO;
+import traininglab.personalrecord.application.data.ExerciseDTO;
+import traininglab.personalrecord.application.data.GetRecordListFilersDTO;
+import traininglab.personalrecord.application.data.RecordEntryDTO;
 
 import java.util.List;
 
 public interface PersonalRecordApplicationService {
 
 	RecordEntryDTO createRecordEntry(CreateRecordRequestDTO data);
+
+	List<RecordEntryDTO> getRecordList(GetRecordListFilersDTO filters);
 
 	List<ExerciseDTO> getExerciseList();
 
