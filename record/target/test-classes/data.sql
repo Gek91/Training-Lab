51fc5a24a3fb26fcd2f1486d6ee6b170679dd76a
Gek91 <gek.pandini@gmail.com>
2023-06-21 07:19:49 +0200

    update gitignore

--- record/target/test-classes/data.sql
+++ record/target/test-classes/data.sql
@@ -1,3 +1,12 @@
+INSERT INTO "user" (id, firstname, lastname, password) values
+('1',	'us1', 'last1', '$2a$10$mIj7rv/YKpUStQHJx1raEOnQzydYLylDfzUetzJfrpTpIXK7myWoi'), -- psw: ciao
+('2',	'us2', 'last2', '$2a$10$mIj7rv/YKpUStQHJx1raEOnQzydYLylDfzUetzJfrpTpIXK7myWoi'),
+('3',	'us3', 'last3', '$2a$10$mIj7rv/YKpUStQHJx1raEOnQzydYLylDfzUetzJfrpTpIXK7myWoi');
+
+INSERT INTO user_role (user_id, role) values
+('1', 'USER'),
+('2', 'USER'),
+('3', 'ADMIN');
 
 INSERT INTO exercise (id, name) values
 ('1',	'ex1'),
@@ -9,6 +18,8 @@ INSERT INTO exercise (id, name) values
 INSERT INTO record_entry (id, user_id, exercise_id, record_date, "value", percentage) values
 (1,	'1',	'1',	'2023-01-01',	100.00, 1.00),
 (2,	'1',	'2',	'2023-01-02',	110.00, 1.00),
-(3,	'1',	'3',	'2023-01-03',	120.00, 1.00);
+(3,	'1',	'3',	'2023-01-03',	120.00, 1.00),
+(4,	'2',	'1',	'2023-01-04',	120.00, 1.00),
+(5,	'3',	'2',	'2023-01-05',	100.00, 1.00);
 
 
